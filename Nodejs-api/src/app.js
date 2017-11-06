ea54b0769c1b859b433c38d77fa1df941e04b1e5
WeslleyFelizardo <weslleyfelizardo1996@gmail.com>
2017-11-06 00:04:05 -0200

    Configurando o CORS

--- Nodejs-api/src/app.js
+++ Nodejs-api/src/app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({
 }));
 app.use(bodyParser.urlencoded({extended: false}));   
 
-app.use(function() {
+app.use( function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
